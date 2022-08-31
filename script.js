a55f14aecdd9c4c7e671af4e6d50ff904cb11512
Victor Ly <ly.victor2000@gmail.com>
2022-08-31 13:32:01 +0200

    counting

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 "use strict";
+
+const array = []; //an empty array that will receive items
+let i = 0; //counter
+
+document.addEventListener("DOMContentLoaded", counting);
+
+function counting() {
+  i++;
+  console.log(i);
+  setTimeout(counting, 1000);
+}
